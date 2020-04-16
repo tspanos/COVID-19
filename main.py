f6be6ffbe8fef3adf4278254954634cfc6df444d
tspanos <19310417+tspanos@users.noreply.github.com>
2020-04-15 20:36:25 -0500

    Update cmdline descriptions

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from worldwide import generate_global_dataset
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="Wide CSSEGISandData Covid-19 to Narrow",
-                                     description='Downloads a ide CSSEGISandData Covid-19 datasets and makes them '
+                                     description='Downloads wide CSSEGISandData Covid-19 datasets and makes them '
                                                  'narrow. Additionally, adds some columns for reporting. '
                                                  'Outputs a CSV.')
-    parser.add_argument('-output', action='store', default='global.csv', required=False, help='Output CSV path.')
+    parser.add_argument('-output', action='store', default='global.csv', required=False, help='Output global CSV path.')
     args = parser.parse_args()
 
     output_path = args.output
